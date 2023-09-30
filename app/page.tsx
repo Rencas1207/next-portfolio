60b3a584eb7465e35ea397569e0043dfbf34857c
Rencas <rencasdag.12@gmail.com>
2023-09-29 21:10:32 -0500

    intro + divider sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Intro from '@/components/Intro';
+import SectionDivider from '@/components/SectionDivider';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+    </main>
   );
 }
